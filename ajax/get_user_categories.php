57d79c02cd25d91837b4ff119b3d0bba811515c4
Pierre de Vésian <pierre.devesian@probesys.com>
2018-06-11 11:00:57 +0200

    First commit

---
+++ ajax/get_user_categories.php
@@ -0,0 +1,20 @@
+<?php
+
+require('../../../inc/includes.php');
+
+header("Content-Type: text/html; charset=UTF-8");
+Html::header_nocache();
+
+Session::checkLoginUser();
+
+if (
+    $_SERVER['REQUEST_METHOD'] == 'POST'
+    && !empty($_POST['requester_user_id'])
+) {
+    $requester_user_id = (int) trim($_POST['requester_user_id']);
+    $user_categories = PluginGroupcategoryGroupcategory::getUserCategories($requester_user_id);
+
+    if (!empty($user_categories)) {
+        echo json_encode($user_categories);
+    }
+}
